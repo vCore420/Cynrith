cd3c4102cccd442162885819d31189f83315d3b6
VCore <jim.hickory1234@gmail.com>
2025-07-31 20:30:57 +1200

    Add files via upload
    Added more enemy NPCs to the first map, added more npc assets for more variety

--- vTileGame/assets/js/charactersData.js
+++ vTileGame/assets/js/charactersData.js
@@ -38,9 +38,9 @@ const NPC_DEFINITIONS = {
         spawns: [
             {
                 map: 0, 
-                x: 26,
-                y: 1,
-                wanderArea: { x1: 26, y1: 1, x2: 28, y2: 3 }
+                x: 2,
+                y: 27,
+                wanderArea: { x1: 1, y1: 25, x2: 4, y2: 28 }
             }
         ],
         dialogue: {
@@ -63,11 +63,11 @@ const NPC_DEFINITIONS = {
 
 // Enemy type definitions
 const ENEMY_TYPES = {
-    slime: {
-        id: "slime",
-        name: "Slime",
-        sprite: "assets/img/enemy/slime.png",
-        moveSpeed: 2,                  // Speed at which the enemy moves
+    slime_01: {
+        id: "slime_01",
+        name: "Groovy Slime",
+        sprite: "assets/img/enemy/slime_01.png",
+        moveSpeed: 1,                  // Speed at which the enemy moves
         distance: 3,                   // Distance the enemy will become hostile to player
         maxHealth: 10,                 // Max health
         attack: 3,                     // Attack damage
@@ -82,13 +82,90 @@ const ENEMY_TYPES = {
                 map: 0, 
                 x: 2,
                 y: 16,
-                wanderArea: { x1: 2, y1: 16, x2: 6, y2: 19 }
+                wanderArea: { x1: 1, y1: 15, x2: 11, y2: 22 }
             },
             {
-                map: 1, 
+                map: 0, 
                 x: 3,
-                y: 8,
-                wanderArea: { x1: 3, y1: 8, x2: 5, y2: 13 }
+                y: 20,
+                wanderArea: { x1: 1, y1: 15, x2: 11, y2: 22 }
+            },
+            {
+                map: 0, 
+                x: 9,
+                y: 17,
+                wanderArea: { x1: 1, y1: 15, x2: 11, y2: 22 }
+            },
+            {
+                map: 0, 
+                x: 24,
+                y: 15,
+                wanderArea: { x1: 22, y1: 12, x2: 28, y2: 28 }
+            },
+            {
+                map: 0, 
+                x: 27,
+                y: 20,
+                wanderArea: { x1: 22, y1: 12, x2: 28, y2: 28 }
+            },
+            {
+                map: 0, 
+                x: 25,
+                y: 27,
+                wanderArea: { x1: 22, y1: 12, x2: 28, y2: 28 }
+            }
+        ]
+    },
+    plant_01: {
+        id: "plant_01",
+        name: "Viscous Plant",
+        sprite: "assets/img/enemy/plant_01.png",
+        moveSpeed: 1,                  // Speed at which the enemy moves
+        distance: 3,                   // Distance the enemy will become hostile to player
+        maxHealth: 15,                 // Max health
+        attack: 4,                     // Attack damage
+        defense: 1,                    // Defense amount
+        speed: 1,                      // Attack speed
+        xpGain: 5,                     // Experience points gained when defeated
+        loot: [                        // Loot items dropped by the enemy
+            { item: "potion", chance: 1, amount: [1, 3] }
+        ],
+        spawns: [
+            {
+                map: 0, 
+                x: 17,
+                y: 7,
+                wanderArea: { x1: 8, y1: 1, x2: 24, y2: 10 }
+            },
+            {
+                map: 0, 
+                x: 14,
+                y: 3,
+                wanderArea: { x1: 8, y1: 1, x2: 24, y2: 10 } 
+            },
+            {
+                map: 0, 
+                x: 9,
+                y: 6,
+                wanderArea: { x1: 8, y1: 1, x2: 24, y2: 10 } 
+            },
+            {
+                map: 0, 
+                x: 20,
+                y: 2,
+                wanderArea: { x1: 8, y1: 1, x2: 24, y2: 10 } 
+            },
+            {
+                map: 0, 
+                x: 21,
+                y: 27,
+                wanderArea: { x1: 6, y1: 25, x2: 24, y2: 28 } 
+            },
+            {
+                map: 0, 
+                x: 11,
+                y: 25,
+                wanderArea: { x1: 6, y1: 25, x2: 24, y2: 28 } 
             }
         ]
     },
