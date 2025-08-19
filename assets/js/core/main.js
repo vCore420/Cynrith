db816caa673c1fbcff396795fbe222a2bf90d5c9
VCore <jim.hickory1234@gmail.com>
2025-08-19 16:37:49 +1200

    Add files via upload

--- assets/js/core/main.js
+++ assets/js/core/main.js
@@ -80,7 +80,7 @@ document.getElementById('btn-a').addEventListener('click', function() {
 
 function updateZoom() {
     if (window.innerHeight > window.innerWidth) {
-        config.zoom = 5; // Zoom in for portrait
+        config.zoom = 1.6; // Zoom in for portrait
     } else {
         config.zoom = 1; // Normal for landscape/desktop
     }
@@ -308,7 +308,7 @@ setInterval(function() {
 
 // On Window Load
 window.onload = function() {
-    updateZoom();
+    updateZoom()
     Setup();
 };
 
