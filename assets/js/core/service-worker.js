6d7454b1fd9296f60baca54ea474e0381f0e14fa
VCore <197873578+vCore420@users.noreply.github.com>
2026-03-14 20:24:05 +1300

    Updated Cache Version

--- assets/js/core/service-worker.js
+++ assets/js/core/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cynrith-cache-v1.1.3"; // Update this on bigger game updateds
+const CACHE_NAME = "cynrith-cache-v1.1.4"; // Update this on bigger game updateds
 const urlsToCache = [
   "/",
   "/index.html",
--- assets/js/ui/titleScreen.js
+++ assets/js/ui/titleScreen.js
@@ -23,7 +23,7 @@ window.addEventListener("DOMContentLoaded", function() {
     document.body.appendChild(overlay);
 
     // Version Watermark
-    const ver = "v1.1.3";  // Set current version here for Title Screen
+    const ver = "v1.1.4";  // Set current version here for Title Screen
 
     const versionWatermark = document.createElement("div");
     versionWatermark.id = "version-watermark";
